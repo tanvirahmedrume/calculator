2f0e3f331af5bf0c37da19fa5f258a4a983da4fe
tanvi-ah <20235203016@cse.bubt.edu.bd>
2024-05-13 11:40:00 +0600

    java work

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView1, textView2,textView3,textView4,textView5,
+            textView6,textView7, textView8,textView9,textView0,result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        textView0 = findViewById(R.id.zero);
+        textView1 = findViewById(R.id.one);
+        textView2 = findViewById(R.id.two);
+        textView3 = findViewById(R.id.three);
+        textView4 = findViewById(R.id.four);
+        textView5 = findViewById(R.id.five);
+        textView6 = findViewById(R.id.six);
+        textView7 = findViewById(R.id.seven);
+        textView8 = findViewById(R.id.eight);
+        textView9 = findViewById(R.id.nine);
+
+        result = findViewById(R.id.result);
     }
 }
